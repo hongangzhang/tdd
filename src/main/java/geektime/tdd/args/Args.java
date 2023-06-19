a7b201012cf209f05bfe5a1e241fccd1c5ab670c
honggang.zhang <honggang.zhang@shopee.com>
2023-06-19 13:03:04 +0800

    tdd demo 消除分支，由于我们无法扩展内建类型 Class 类，因此只能使用“抽象工厂”模式（Abstract Factory）的变体来替换分支

--- src/main/java/geektime/tdd/args/Args.java
+++ src/main/java/geektime/tdd/args/Args.java
@@ -4,8 +4,13 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class Args {
+
+    private static final Map<Class<?>, OptionParser> PARSERS = Map.of(boolean.class, new BooleanOptionParser(),
+                                                                      int.class, new IntOptionParser(),
+                                                                      String.class, new StringOptionParser());
 
     public static <T> T parse(Class<T> optionsClass, String... args) {
 
@@ -23,54 +28,7 @@ public class Args {
     }
 
     private static Object parseOption(List<String> arguments, Parameter parameter) {
-        return getOptionParser(parameter.getType()).parse(arguments, parameter.getAnnotation(Option.class));
-    }
-
-    private static OptionParser getOptionParser(Class<?> type) {
-        OptionParser parser = null;
-
-        if (type.equals(boolean.class)) {
-            parser = new BooleanOptionParser();
-        }
-
-        if (type.equals(int.class)) {
-            parser = new IntOptionParser();
-        }
-
-        if (type.equals(String.class)) {
-            parser = new StringOptionParser();
-        }
-        return parser;
-    }
-
-    interface OptionParser {
-        Object parse(List<String> arguments, Option option);
-    }
-
-    static class StringOptionParser implements OptionParser {
-
-        @Override
-        public Object parse(List<String> arguments, Option option) {
-            int index = arguments.indexOf("-" + option.value());
-            return arguments.get(index + 1);
-        }
-    }
-
-    static class IntOptionParser implements OptionParser {
-
-        @Override
-        public Object parse(List<String> arguments, Option option) {
-            int index = arguments.indexOf("-" + option.value());
-            return Integer.parseInt(arguments.get(index + 1));
-        }
-    }
-
-    static class BooleanOptionParser implements OptionParser {
-
-        @Override
-        public Object parse(List<String> arguments, Option option) {
-            return arguments.contains("-" + option.value());
-        }
+        return PARSERS.get(parameter.getType()).parse(arguments, parameter.getAnnotation(Option.class));
     }
 
 }
