0d34264d07315dada4657d972837febd1cdee0b2
honggang.zhang <honggang.zhang@shopee.com>
2023-06-19 13:36:36 +0800

    tdd demo 消除重复-消除重复

--- src/main/java/geektime/tdd/args/IntOptionParser.java
+++ src/main/java/geektime/tdd/args/IntOptionParser.java
@@ -1,13 +1,24 @@
 package geektime.tdd.args;
 
 import java.util.List;
+import java.util.function.Function;
 
 class IntOptionParser implements OptionParser {
+
+    Function<String, Object> valueParser = Integer::parseInt;
+
+    public IntOptionParser() {
+    }
+
+    public IntOptionParser(Function<String, Object> valueParser) {
+        this.valueParser = valueParser;
+    }
 
     @Override
     public Object parse(List<String> arguments, Option option) {
         int index = arguments.indexOf("-" + option.value());
         String value = arguments.get(index + 1);
-        return Integer.parseInt(value);
+        return valueParser.apply(value);
     }
+
 }
--- src/main/java/geektime/tdd/args/StringOptionParser.java
+++ src/main/java/geektime/tdd/args/StringOptionParser.java
@@ -1,13 +1,9 @@
 package geektime.tdd.args;
 
-import java.util.List;
+class StringOptionParser extends IntOptionParser {
 
-class StringOptionParser implements OptionParser {
-
-    @Override
-    public Object parse(List<String> arguments, Option option) {
-        int index = arguments.indexOf("-" + option.value());
-        String value = arguments.get(index + 1);
-        return String.valueOf(value) ;
+    public StringOptionParser() {
+        super(String::valueOf);
     }
+
 }
