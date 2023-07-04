01c96a4ba3a45112dcad918cfc92f1ae3a5175a7
honggang.zhang <honggang.zhang@shopee.com>
2023-07-04 12:54:59 +0800

    tdd demo 调整任务列表-测试粒度选择更小的-BooleanOptionParse解析测试

--- src/test/java/geektime/tdd/args/BooleanOptionParserTest.java
+++ src/test/java/geektime/tdd/args/BooleanOptionParserTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.lang.annotation.Annotation;
+import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,37 +12,9 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BooleanOptionParserTest {
 
-    // sad path:
-    @Test
-    public void should_not_accept_extra_arguments_for_boolean_option() {
-        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class, () -> {
-            new BooleanOptionParser().parse(asList("-l", "t"), option("l"));
-        });
-
-        assertEquals("l", e.getOption());
-    }
-
-    // default value:
-    @Test
-    public void should_set_default_value_if_option_not_present(){
-        Assertions.assertFalse(new BooleanOptionParser().parse(asList(), option("l")));
-    }
-
-
-    private Option option(String value) {
+    public static Option option(String value) {
         return new Option() {
 
-            /**
-             * Returns the annotation interface of this annotation.
-             *
-             * @return the annotation interface of this annotation
-             * @apiNote Implementation-dependent classes are used to provide
-             * the implementations of annotations. Therefore, calling {@link
-             * Object#getClass getClass} on an annotation will return an
-             * implementation-dependent class. In contrast, this method will
-             * reliably return the annotation interface of the annotation.
-             * @see Enum#getDeclaringClass
-             */
             @Override
             public Class<? extends Annotation> annotationType() {
                 return Option.class;
@@ -52,6 +25,22 @@ public class BooleanOptionParserTest {
                 return value;
             }
         };
+    }
+
+    // sad path:
+    @Test
+    public void should_not_accept_extra_arguments_for_boolean_option() {
+        TooManyArgumentsException e = assertThrows(TooManyArgumentsException.class,
+                                                   () -> new BooleanOptionParser().parse(asList("-l", "t"),
+                                                                                         option("l")));
+
+        assertEquals("l", e.getOption());
+    }
+
+    // default value:
+    @Test
+    public void should_set_default_value_if_option_not_present() {
+        Assertions.assertFalse(new BooleanOptionParser().parse(List.of(), option("l")));
     }
 
 
