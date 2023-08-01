b8e028f6cb9fb21eefa3e4978a7d617df2088a21
honggang.zhang <honggang.zhang@shopee.com>
2023-08-01 12:48:49 +0800

    tdd 05｜TDD中的测试（1）：状态验证为什么是主要的使用方式？：1.初始化。主要是设置测试上下文，从而使待测系统（System Under Test）处于可测试的状态。例如，对于需要操作数据库的后台系统，测试上下文包含了已经灌注测试数据的测试用数据库，并将其与待测系统连接。2.执行测试。就是按照测试脚本的描述与待测系统互动。例如，按照功能描述，通过 API 对系统进行相应的操作。3.验证结果。就是验证待测系统是否处于我们期待的状态中。例如，经过测试，数据库中的业务数据是否发生了期待中的改变。4.复原。就是将测试上下文、待测系统复原回测试之前的状态，或者消除测试对于待测系统的副作用。例如，删除测试数据中的数据（通常是通过事务回滚）。

--- src/test/java/geektime/tdd/args/ArgsTest.java
+++ src/test/java/geektime/tdd/args/ArgsTest.java
@@ -9,19 +9,38 @@ public class ArgsTest {
 
     @Test
     public void should_parse_multi_option() {
+
+        // SUT Args.parse
+
+        // exercise
         MultiOptions multiOptions = Args.parse(MultiOptions.class, "-l", "-p", "8080", "-d", "/usr/logs");
+
+        // verify
         assertTrue(multiOptions.logging());
         assertEquals(8080, multiOptions.port());
         assertEquals("/usr/logs", multiOptions.directory());
+
+        // teardown
     }
 
-    @Test
-    public void should_throw_illegal_option_exception_if_annotation_not_present() {
-        IllegalOptonException e = assertThrows(IllegalOptonException.class,
-                                               () -> Args.parse(OptionsWithoutAnnotation.class, "-l", "-p", "8080",
-                                                                "-d", "/usr/logs"));
+    // setup
+    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
+    }
 
+
+    @Test
+    public void should_raise_exception_if_annotation_not_present()   {
+        IllegalOptonException e = /*verify*/ assertThrows(IllegalOptonException.class,
+                                               () -> /*exercise*/ Args.parse(OptionsWithoutAnnotation.class, "-l", "-p", "8080",
+                                                                "-d", "/usr/logs"));
+        // verify
         assertEquals("port", e.getParameter());
+
+        // teardown
+    }
+
+    // setup
+    static record OptionsWithoutAnnotation(@Option("l") boolean logging, int port, @Option("d") String directory) {
     }
 
     @Test
@@ -31,12 +50,6 @@ public class ArgsTest {
         assertArrayEquals(new Integer[]{1, 2, -3, 5}, listOptions.decimals());
     }
 
-    static record MultiOptions(@Option("l") boolean logging, @Option("p") int port, @Option("d") String directory) {
-    }
-
-
-    static record OptionsWithoutAnnotation(@Option("l") boolean logging, int port, @Option("d") String directory) {
-    }
 
     static record ListOptions(@Option("g") String[] group, @Option("d") Integer[] decimals) {
     }
